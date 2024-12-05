4cda8424f44d8edb58c7a3eb6b674e54e9ceaf56
Aditir06 <aditiraut80266@gmail.com>
2024-12-05 15:39:37 +0530

    Update 26_.c

--- 26_.c
+++ 26_.c
@@ -6,7 +6,7 @@ int main()
     printf("enter the number of rows:");
     scanf("%d",&rows);
     for(i=1; i<=rows; ++i){
-        for(i=1; j<=i; ++j){
+        for(j=1; j<=i; ++j){
             printf("* ");
         }
         printf("\n");
